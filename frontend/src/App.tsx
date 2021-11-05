82e2ae7accc5dc42653f9ac9715927a1355cc319
yeppin <yebins49@gmail.com>
2021-11-05 19:52:56 +0900

    feat: tsx setting

---
+++ frontend/src/App.tsx
@@ -0,0 +1,12 @@
+import React from "react";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+
+function App() {
+  return (
+    <div className="App">
+      <div>App pages</div>
+    </div>
+  );
+}
+
+export default App;
