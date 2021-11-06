da765ad52347688187366426255edc0c89ae07bf
young924 <ahenaka9@gmail.com>
2021-11-06 15:27:11 +0900

    Change http method restriction method

--- backend/account/views.py
+++ backend/account/views.py
@@ -1,24 +1,22 @@
 from django.http import JsonResponse, HttpResponseBadRequest
-from django.http.response import HttpResponseNotAllowed
+from django.views.decorators.http import require_http_methods
 
 import json
 from json.decoder import JSONDecodeError
 
 from .models import User
 
+@require_http_methods(["POST"])
 def signup(request):
-    if request.method == 'POST':
-        try:
-            body = request.body.decode()
-            email = json.loads(body)['email']
-            username = json.loads(body)['username']
-            password = json.loads(body)['password']
-        except (KeyError, JSONDecodeError):
-            return HttpResponseBadRequest()
-        user = User.objects.create_user(email=email, username=username)
-        user.set_password(password)
-        user.save()
-        response_dict = {"id": user.id, "email": user.email, "username": user.username }
-        return JsonResponse(response_dict, status=201)
-    else:
-        return HttpResponseNotAllowed(['POST'])
+    try:
+        body = request.body.decode()
+        email = json.loads(body)['email']
+        username = json.loads(body)['username']
+        password = json.loads(body)['password']
+    except (KeyError, JSONDecodeError):
+        return HttpResponseBadRequest()
+    user = User.objects.create_user(email=email, username=username)
+    user.set_password(password)
+    user.save()
+    response_dict = {"id": user.id, "email": user.email, "username": user.username }
+    return JsonResponse(response_dict, status=201)
