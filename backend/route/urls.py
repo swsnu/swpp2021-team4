f675095edd2c0c2dbe5ac5bffde1cf751a19903b
yeppin <yebins49@gmail.com>
2021-11-08 04:42:41 +0900

    hotfix: fix minor errors in urls.py and refactor views.py

--- backend/route/urls.py
+++ backend/route/urls.py
@@ -3,12 +3,12 @@ from route import views
 
 urlpatterns = [
     path('post/', views.posts, name='post'),
-    path('post/:id/', views.post_spec, name='post_spec'),
-    path('post/:id/cart/:fid/', views.post_cart, name='post_cart'),
-    path('post/:id/like/', views.post_like, name='post_like'),
-    path('post/:id/comment/', views.post_comment, name='post_comment'),
-    path('post/:id/comment/:cid', views.post_comment_spec, name='post_comment_spec'),
+    path('post/<int:id>/', views.post_spec, name='post_spec'),
+    path('post/<int:id>/cart/<int:fid>/', views.post_cart, name='post_cart'),
+    path('post/<int:id>/like/', views.post_like, name='post_like'),
+    path('post/<int:id>/comment/', views.post_comment, name='post_comment'),
+    path('post/<int:id>/comment/<int:cid>', views.post_comment_spec, name='post_comment_spec'),
     path('place/', views.place_create, name='place_create'),
-    path('place/:id/', views.place_spec, name='place_spec'),
-    path('place/:id/cart/:fid/', views.place_cart, name='place_cart')
+    path('place/<int:id>/', views.place_spec, name='place_spec'),
+    path('place/<int:id>/cart/<int:fid>/', views.place_cart, name='place_cart')
 ]
--- backend/route/views.py
+++ backend/route/views.py
@@ -13,11 +13,11 @@ def posts(request):
         for post in Post.objects.all():
             comments=[]
             for comment in post.comment_set.all():
-                comments.append({'content': comment.content, 'author_id':comment.author.id})
+                comments.append({'content': comment.content, 'author_id':comment.author_id})
             folder_id=''
             if post.folder:
-                folder_id=post.folder.id
-            postlist.append({'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image.url, 'thumbnail_image': post.thumbnail_image.url, 
+                folder_id=post.folder_id
+            postlist.append({'title': post.title, 'author_id': post.author_id, 'header_img': post.header_image.url, 'thumbnail_image': post.thumbnail_image.url, 
             'days': post.days, 'folder_id': folder_id, 'is_shared':post.is_shared, 'theme':post.theme, 'comment': comments, 'season': post.season, 
             'location': post.location, 'availableWithOutCar': post.availableWithoutCar})
         if len(postlist)==0:
@@ -42,13 +42,13 @@ def posts(request):
         except (KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
         folder=Folder.objects.get(id=post_folder_id)
-        post = Post(title=post_title, author=request.user, folder=folder, header_image=post_header_img, thumbnail_image=post_thumbnail_img,days=post_days, 
+        post = Post(title=post_title, author=request.user, folder=folder, header_image=post_header_image, thumbnail_image=post_thumbnail_image,days=post_days, 
         is_shared=post_is_shared,location=post_location, theme=post_theme, season=post_season, availableWithoutCar=post_availableWithOutCar)
         post.save()
         folder_id=''
         if post.folder:
-            folder_id=post.folder.id
-        response_dict = {'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
+            folder_id=post.folder_id
+        response_dict = {'title': post.title, 'author_id': post.author_id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
             'days': post.days, 'folder_id': folder_id, 'is_shared':post.is_shared, 'theme':post.theme, 'season': post.season, 
             'location': post.location, 'availableWithOutCar': post.availableWithoutCar}
         return JsonResponse(response_dict, status=201)
@@ -58,10 +58,10 @@ def post_spec(request, id):
     if request.method=="GET":
         post = Post.objects.get(id=id)
         comments=[]
-        for comment in post.comment_set.all:
-            comments.append({'content': comment.content, 'author_id':comment.author.id})
-        response_dict = {'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
-            'days': post.days, 'folder_id': post.folder.id, 'is_shared':post.is_shared, 'theme':post.theme, 'comment': comments, 'season': post.season, 
+        for comment in post.comment_set.all():
+            comments.append({'content': comment.content, 'author_id':comment.author_id})
+        response_dict = {'title': post.title, 'author_id': post.author_id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
+            'days': post.days, 'folder_id': post.folder_id, 'is_shared':post.is_shared, 'theme':post.theme, 'comment': comments, 'season': post.season, 
             'location': post.location, 'availableWithOutCar': post.availableWithoutCar}
         return JsonResponse(response_dict, safe=False)
     elif request.method=='PUT':
@@ -83,11 +83,11 @@ def post_spec(request, id):
             post_availableWithOutCar=body['availableWithOutCar']
         except (KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
-        post = Post(title=post_title, author=request.user, folder_id=post_folder, header_image=post_header_img, thumbnail_image=post_thumbnail_img,days=post_days, 
+        post = Post(title=post_title, author=request.user, folder_id=post_folder_id, header_image=post_header_image, thumbnail_image=post_thumbnail_image,days=post_days, 
         is_shared=post_is_shared, location=post_location, theme=post_theme, season=post_season, availableWithoutCar=post_availableWithOutCar)
         post.save()
-        response_dict = {'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
-            'days': post.days, 'folder_id': post.folder.id, 'is_shared':post.is_shared, 'theme':post.theme, 'season': post.season, 
+        response_dict = {'title': post.title, 'author_id': post.author_id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
+            'days': post.days, 'folder_id': post.folder_id, 'is_shared':post.is_shared, 'theme':post.theme, 'season': post.season, 
             'location': post.location, 'availableWithOutCar': post.availableWithoutCar}
         return JsonResponse(response_dict, status=201)
     else: #delete
@@ -115,19 +115,19 @@ def post_cart(request,id, fid):
     except (KeyError, JSONDecodeError) as e:
         return HttpResponseBadRequest()
     if request.method=='POST':
-        post = Post(title=post_title, author=request.user, folder_id=fid, header_image=post_header_img, thumbnail_image=post_thumbnail_img,days=post_days, 
+        post = Post(title=post_title, author=request.user, folder_id=fid, header_image=post_header_image, thumbnail_image=post_thumbnail_image,days=post_days, 
         is_shared=post_is_shared, theme=post_theme, season=post_season, location=post_location, availableWithoutCar=post_availableWithOutCar)
         post.save()
-        response_dict = {'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
-        'days': post.days, 'folder_id': post.folder.id, 'is_shared':post.is_shared, 'theme':post.theme, 'season': post.season, 
+        response_dict = {'title': post.title, 'author_id': post.author_id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
+        'days': post.days, 'folder_id': post.folder_id, 'is_shared':post.is_shared, 'theme':post.theme, 'season': post.season, 
         'location': post.location, 'availableWithOutCar': post.availableWithoutCar}
         return JsonResponse(response_dict, status=201)
     elif request.method=='DELETE':
-        post = Post(title=post_title, author=request.user, folder=None, header_image=post_header_img, thumbnail_image=post_thumbnail_img,days=post_days, 
+        post = Post(title=post_title, author=request.user, folder=None, header_image=post_header_image, thumbnail_image=post_thumbnail_image,days=post_days, 
         is_shared=post_is_shared, theme=post_theme, season=post_season, location=post_location, availableWithoutCar=post_availableWithOutCar)
 
 @require_http_methods(["POST", "DELETE"])
-def post_like(reqeust, id):
+def post_like(request, id):
     logged_user_id=request.session.get('user', None)
     if not logged_user_id:
             return HttpResponse(status=405)
@@ -135,8 +135,9 @@ def post_like(reqeust, id):
         post = Post.objects.get(id=id)
         like_list = post.like_set.filter(user_id=request.user.id)
         Like.objects.create(user=request.user, post=post)
-        return JsonResponse({'postLikeUserCount': post.like_set.count()}, status=201)
+        return JsonResponse({'postLikeUserCount': like_list.count()}, status=201)
     elif request.method=='DELETE':
+        post = Post.objects.get(id=id)
         post.like_set.get(user=request.user).delete()
         return HttpResponse(status=200)
 
@@ -145,8 +146,8 @@ def post_comment(request, id):
     if request.method=='GET':
         post=Post.objects.get(id=id)
         comments=[]
-        for comment in post.comment_set.all:
-            comments.append({'content': comment.content, 'author_id':comment.author.id})
+        for comment in post.comment_set.all():
+            comments.append({'content': comment.content, 'author_id':comment.author_id})
         return JsonResponse(comments, safe=False)
     else: #POST  
         logged_user_id=request.session.get('user', None)
@@ -164,7 +165,7 @@ def post_comment(request, id):
             {
                 'id': comment.id,
                 'content':comment.content,
-                'author_id': comment.author.id,
+                'author_id': comment.author_id,
                 'created_at': comment.created_at.strftime("%Y. %m. %d. %H:%M")                
             }, status=201
         )      
@@ -183,7 +184,7 @@ def post_comment_spec(request, id, cid):
             return HttpResponseBadRequest()
         search_comment.content=comment_content
         search_comment.save()
-        response_dict = {'id': search_comment.id, 'post_id': id, 'content':search_comment.content, 'author':search_comment.author.id}
+        response_dict = {'id': search_comment.id, 'post_id': id, 'content':search_comment.content, 'author':search_comment.author_id}
         return JsonResponse(response_dict, status=200)
     else: #delete
         Comment.objects.get(id=cid).delete() 
@@ -191,6 +192,7 @@ def post_comment_spec(request, id, cid):
 
 @require_http_methods(["POST"])
 def place_create(request):
+    logged_user_id=request.session.get('user', None)
     if not logged_user_id:
         return HttpResponse(status=405)
     else: #POST
@@ -202,16 +204,16 @@ def place_create(request):
             day = body['day']
         except (KeyError, JSONDecodeError) as e:
             return HttpResponseBadRequest()
-        place=Place(post_id=post_id, place_id=place_id, description=descriptoin, day=day)
+        place=Place(post_id=post_id, place_id=place_id, description=description, day=day)
         place.save()
-        response_dict = {'post': place.post.id, 'place':place.place.id, 'description': place.description, 'day': place.day}
+        response_dict = {'post': place.post_id, 'place':place.place_id, 'description': place.description, 'day': place.day}
         return JsonResponse(response_dict, status=201)
 
 @require_http_methods(["GET", "PUT", "DELETE"])
 def place_spec(request, id):
     if request.method=="GET":
         place = Place.objects.get(id=id)
-        response_dict = {'post_id': place.post.id, 'place_id': place.place.id, 'description': place.description, 'day':place.day, 'folder_id': place.folder.id}
+        response_dict = {'post_id': place.post_id, 'place_id': place.place_id, 'description': place.description, 'day':place.day, 'folder_id': place.folder_id}
         return JsonResponse(response_dict, safe=False)
     elif request.method=='PUT':
         logged_user_id=request.session.get('user', None)
@@ -229,7 +231,7 @@ def place_spec(request, id):
             return HttpResponseBadRequest()
         place = Place( post_id = post_id, place_id = place_id, folder_id = folder_id, description= description, day= day)
         place.save()
-        response_dict = {'post_id': place.post.id, 'place_id': place.place.id, 'folder_id': place.folder.id, 'description': place.description, 'day':place.day}
+        response_dict = {'post_id': place.post_id, 'place_id': place.place_id, 'folder_id': place.folder_id, 'description': place.description, 'day':place.day}
         return JsonResponse(response_dict, status=201)
     else: #delete
         logged_user_id=request.session.get('user', None)
@@ -253,7 +255,7 @@ def place_cart(request,id, fid):
     if request.method=='POST':
         place = Place(post_id=post_id, place_id=place_id, description=description, folder_id=folder_id, day=day)
         place.save()
-        response_dict = {'post_id': place.post.id, 'place_id': place.place.id, 'folder_id': place.folder.id, 'description': place.description, 'day':place.day}
+        response_dict = {'post_id': place.post_id, 'place_id': place.place_id, 'folder_id': place.folder_id, 'description': place.description, 'day':place.day}
         return JsonResponse(response_dict, status=201)
     elif request.method=='DELETE':
         place = Place(post_id=post_id, place_id=place_id, description=description, folder=None, day=day)
