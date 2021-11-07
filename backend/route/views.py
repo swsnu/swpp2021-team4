e031a7f97c8f4f1c888e1b8034e09310e6e86a7c
mina <dbmina@naver.com>
2021-11-07 11:24:58 +0900

    add serializers.py

--- backend/route/views.py
+++ backend/route/views.py
@@ -14,7 +14,7 @@ def posts(request):
             comments=[]
             for comment in post.comment_set.all:
                 comments.append({'content': comment.content, 'author_id':comment.author.id})
-            postlist.append({'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image, 'thumbnail_image': post.thumbnail_image, 
+            postlist.append({'title': post.title, 'author_id': post.author.id, 'header_img': post.header_image.url, 'thumbnail_image': post.thumbnail_image.url, 
             'days': post.days, 'folder_id': post.folder.id, 'is_shared':post.is_shared, 'theme':post.theme, 'comment': comments, 'season': post.season, 
             'location': post.location, 'availableWithOutCar': post.availableWithoutCar})
         if len(postlist)==0:
