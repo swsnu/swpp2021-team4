4103bdbc75acb9e054d965a3334370e29f1fd707
yeppin <yebins49@gmail.com>
2021-11-08 02:08:09 +0900

    feat: implement variable style in navbar

--- frontend/src/components/NavBar.tsx
+++ frontend/src/components/NavBar.tsx
@@ -1,7 +1,7 @@
 import React from "react";
-
+import "../styles/NavBar.css";
 function NavBar() {
-  return <div>NavBar component</div>;
+  return <div className="navbar">NavBar component</div>;
 }
 
 export default NavBar;
