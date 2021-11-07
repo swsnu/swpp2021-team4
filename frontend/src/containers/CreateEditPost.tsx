6d6fe6d81ca6ccea57742d2f028857761209791b
kjw7953 <uis7953@naver.com>
2021-11-07 15:24:08 +0900

    merge

---
+++ frontend/src/containers/CreateEditPost.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function CreateEditPost() {
+  return <div>CreateEditPost container</div>;
+}
+
+export default CreateEditPost;
---
+++ frontend/src/containers/SIgnup.tsx
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Signup() {
+  return <div>Signup container</div>;
+}
+
+export default Signup;
