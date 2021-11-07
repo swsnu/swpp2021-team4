b442353f7b8fd2973e5bf3dcb382f8aa146f7aa5
Jeong-Woo-Kim <53823950+kjw7953@users.noreply.github.com>
2021-11-07 15:43:16 +0900

    merge (#42)

---
+++ frontend/src/pages/SigninPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import NavBar from "../components/NavBar";
+import Signin from "../containers/Signin";
+
+function SigninPage() {
+  return (
+    <>
+      <NavBar />
+      <Signin />
+    </>
+  );
+}
+
+export default SigninPage;
