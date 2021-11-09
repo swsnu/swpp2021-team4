f56b5fa113bef74525a20922e363b4e801c3d9bf
yeppin <yebins49@gmail.com>
2021-11-09 17:42:48 +0900

    fix: fix getPostAction reducer

--- frontend/src/store/Post/postReducer.ts
+++ frontend/src/store/Post/postReducer.ts
@@ -1,44 +1,43 @@
-import {
-  GET_POST_SUCCESS,
-  GET_POST_FAIL
-} from '../actionTypes';
+import { GET_POST_SUCCESS, GET_POST_FAIL } from "../actionTypes";
 
-import {
-  PostDispatchType,
-  PostType
-} from './postInterfaces';
+import { PostDispatchType, PostType } from "./postInterfaces";
 
 export type PostStateType = {
-  detailedPost: PostType
-}
+  detailedPost: PostType;
+};
 
 const initialState: PostStateType = {
   detailedPost: {
-    title: '',
+    title: "",
     author: {
       id: 0,
-      email: '',
-      username: '',
-      profileImage: '',
-      folders: []
+      email: "",
+      username: "",
+      profileImage: "",
+      folders: [],
     },
-    folderId: 0, // folderName 바로 알려줄 수는 없는지?
+    folderId: 0,
+    folderName: "",
     //location은 어디서 받아오는지?
     days: 0,
-    headerImage: '',
-    thumbnailImage: '',
-    createdAt: '',
-    updatedAt: '',
-    theme: '',// lover로 렌더됨
-    season: '',
+    location: "",
+    headerImage: "",
+    thumbnailImage: "",
+    createdAt: "",
+    updatedAt: "",
+    theme: "", // lover로 렌더됨
+    season: "",
     //Season여러 개 받도록 해야 하지 않는지?
     //sum으로 렌더됨
     isShared: false,
     availableWithoutCar: false,
-  }
-}
+  },
+};
 
-export default (state: PostStateType = initialState, action: PostDispatchType): PostStateType => {
+export default (
+  state: PostStateType = initialState,
+  action: PostDispatchType
+): PostStateType => {
   switch (action.type) {
     case GET_POST_SUCCESS:
       return { ...state, detailedPost: action.payload };
@@ -47,4 +46,4 @@ export default (state: PostStateType = initialState, action: PostDispatchType): 
     default:
       return { ...state };
   }
-}
+};
