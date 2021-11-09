296acf265a322f03441f94b100e19d15db508f3d
yeppin <yebins49@gmail.com>
2021-11-09 17:43:22 +0900

    feat: enhance PostDetailPage feature

--- frontend/src/containers/PostDetail.tsx
+++ frontend/src/containers/PostDetail.tsx
@@ -6,28 +6,40 @@ import { getPostAction } from "../store/Post/postAction";
 import "../styles/PostDetail.css";
 
 function PostDetail() {
+  interface String {
+    id: string;
+  }
   const dispatch = useDispatch();
-  const { id } = useParams<string>();
+  const { id } = useParams<String>();
   useEffect(() => {
     dispatch(getPostAction(Number(id)));
   }, [dispatch, id]);
   const post = usePostState();
+  console.log(post);
   const withoutCar: boolean = post.availableWithoutCar;
   return (
     <div className="post-detail-header">
-      <div className="header-top">
-        <div className="post-folder-name">Folder Name</div>
-        <div className={`without-car-checkbox-${withoutCar}`}>
-          Available without car!
-        </div>
+      <div className="header-image">
+        <img src="https://media.triple.guide/triple-cms/c_limit,f_auto,h_1024,w_1024/73968eea-cbbe-49cd-b001-353e9e962cbf.jpeg" />
       </div>
-      <div className="header-middle">
-        <div className="post-title-name">{post.title}</div>
-        <div className="post-info-container">
-          <div className="post-info info-button">GyeonJu</div>
-          <div className="post-info info-button">{post.days}</div>
-          <div className="post-info info-button">{post.season}</div>
-          <div className="post-info info-button">{post.theme}</div>
+      <div className="header-content">
+        <div className="header-top">
+          <div className="post-folder-name">{post.folder_name}</div>
+          <div className={`without-car-checkbox-${withoutCar}`}>
+            Available without car!
+          </div>
+        </div>
+        <div className="header-middle">
+          <div className="post-title-name">{post.title}</div>
+        </div>
+        <div className="header-bottom">
+          <div className="post-author">{post.author_name}</div>
+          <div className="post-info-container">
+            <div className="post-info info-button">{post.location}</div>
+            <div className="post-info info-button">{post.days}일 코스</div>
+            <div className="post-info info-button">{post.season}</div>
+            <div className="post-info info-button">{post.theme}</div>
+          </div>
         </div>
       </div>
     </div>
--- frontend/src/hooks/usePostState.tsx
+++ frontend/src/hooks/usePostState.tsx
@@ -1,30 +1,35 @@
 import { useSelector } from "react-redux";
-import { RootState } from "../store/store";
+import { RootReducerType } from "../store/store";
 import { PostStateType } from "../store/Post/postReducer";
 
 const initialState: PostStateType = {
-    detailedPost: {
-      title: '',
-      author: {
-        id: 0,
-        email: '',
-        username: '',
-        profileImage: '',
-        folders: []
-      },
-      folderId: 0,
-      days: 0,
-      headerImage: '',
-      thumbnailImage: '',
-      createdAt: '',
-      updatedAt: '',
-      theme: '',
-      season: '',
-      isShared: false,
-      availableWithoutCar: false,
-    }
-  }
+  detailedPost: {
+    title: "",
+    author: {
+      id: 0,
+      email: "",
+      username: "",
+      profileImage: "",
+      folders: [],
+    },
+    folderId: 0,
+    folderName: "",
+    days: 0,
+    location: "",
+    headerImage: "",
+    thumbnailImage: "",
+    createdAt: "",
+    updatedAt: "",
+    theme: "",
+    season: "",
+    isShared: false,
+    availableWithoutCar: false,
+  },
+};
 export const usePostState = () => {
-  const post = useSelector((state: RootState) => state.post.detailedPost || initialState.detailedPost);
+  const post = useSelector(
+    (state: RootReducerType) =>
+      state.post.detailedPost || initialState.detailedPost
+  );
   return post;
 };
