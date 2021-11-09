c45592ecba98c91eab89bcc5e5862d9be01eb75b
young924 <ahenaka9@gmail.com>
2021-11-09 14:28:32 +0900

    Add user_info test except form

--- backend/account/tests.py
+++ backend/account/tests.py
@@ -1,9 +1,10 @@
 from django.test import TestCase, Client
 from django.core.files import File
+from django.core.files.uploadedfile import SimpleUploadedFile
 from django.utils import timezone
-
 import json
 from .models import User
+from .forms import UserForm
 
 class AccountTestCase(TestCase):
     def setUp(self):
@@ -107,3 +108,38 @@ class AccountTestCase(TestCase):
         
         response = client.post('/user/signout/')
         self.assertEqual(response.status_code, 204)
+
+    def test_user_info(self):
+        client = Client()
+        user = User.objects.get(email="swpp@swpp.com")
+
+        response = client.post(f'/user/{user.id}/')
+        self.assertEqual(response.status_code, 405)
+
+        response = client.get('/user/112312/')
+        self.assertEqual(response.status_code, 401)
+
+        response = client.get(f'/user/{user.id}/')
+        self.assertIn("swpp@swpp.com", response.content.decode())
+
+        user.profile_image = File(open("./test_img.jpeg", "rb"))
+        user.save()
+        path = user.profile_image.path
+        response = client.get(f'/user/{user.id}/')
+        self.assertIn(timezone.now().strftime('%Y/%m/%d'), path)
+
+        user.profile_image = None
+        user.save()
+
+        form_data = {'wrong_data': 'wrong'}
+        form = UserForm(data=form_data)
+        self.assertFalse(form.is_valid())
+
+        # upload_file = open("./test_img.jpeg", "rb")
+        # post_dict = {
+        #     'username': 'edited_username',
+        #     'password': 'edited_password',
+        # }
+        # file_dict = {'file': SimpleUploadedFile(upload_file.name, upload_file.read())}
+        # form = UserForm(post_dict, file_dict)
+        # self.assertTrue(form.is_valid())
