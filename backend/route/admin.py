e861272551cc6c89a029ed38841ac6df78d8575f
yeppin <yebins49@gmail.com>
2021-11-09 17:41:06 +0900

    add: add models to admin.py

--- backend/route/admin.py
+++ backend/route/admin.py
@@ -1,5 +1,10 @@
 from django.contrib import admin
-from .models import Post
+from .models import Folder, Post, Place, Comment, Like, Path
 # Register your models here.
 
+admin.site.register(Folder)
 admin.site.register(Post)
+admin.site.register(Place)
+admin.site.register(Comment)
+admin.site.register(Like)
+admin.site.register(Path)
