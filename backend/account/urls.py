f0e0048621f7c867554a8c059362286ba2c14820
young924 <ahenaka9@gmail.com>
2021-11-09 14:28:18 +0900

    Implement user_info on account

--- backend/account/urls.py
+++ backend/account/urls.py
@@ -6,4 +6,5 @@ urlpatterns = [
     path('signup/', views.signup, name='signup'), 
     path('signin/', views.signin, name='signin'), 
     path('signout/', views.signout, name='signout'), 
+    path('<int:user_id>/', views.user_info, name='user_info')
 ]
--- backend/account/views.py
+++ backend/account/views.py
@@ -2,11 +2,13 @@ from django.http import HttpResponse, JsonResponse, HttpResponseBadRequest
 from django.views.decorators.http import require_http_methods
 from django.contrib.auth import logout
 from django.contrib.auth.hashers import check_password
+from django.views.decorators.csrf import csrf_exempt
 
 import json
 from json.decoder import JSONDecodeError
 
 from .models import User
+from .forms import UserForm
 
 @require_http_methods(["POST"])
 def signup(request):
@@ -66,3 +68,39 @@ def signout(request):
         return HttpResponse(status=401)
     logout(request)
     return HttpResponse(status=204)
+
+@csrf_exempt
+@require_http_methods(["GET", "PUT"])
+def user_info(request, user_id):
+    try:
+        user = User.objects.get(id=user_id)
+    except User.DoesNotExist:   # Wrong id
+        return HttpResponse(status=401)
+
+    if request.method == 'GET':
+        if user.profile_image:
+            profile_image = user.profile_image.url
+        else:
+            profile_image = None
+        response_dict = {
+            'email': user.email,
+            'username': user.username,
+            'profile_image': profile_image
+        }
+        return JsonResponse(response_dict, safe=False)
+    if request.method == 'PUT':
+        form = UserForm(request.POST, request.FILES)
+        if form.is_valid():
+            user.username = form.cleaned_data['username']
+            user.profile_image = form.cleaned_data['profile_image']
+            user.set_password(form.cleaned_data['password'])
+            user.save()
+            user.update_date()
+            response_dict = {
+                'email': user.email,
+                'username': user.username,
+                'profile_image': user.profile_image.url
+            }
+            return JsonResponse(response_dict, safe=False)
+        else:
+            return HttpResponse(status=400)
