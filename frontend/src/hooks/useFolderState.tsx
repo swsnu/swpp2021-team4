d8262bc76b4dc2ae5ad854f33a60a7ccc402e47a
yeppin <yebins49@gmail.com>
2021-11-11 07:57:25 +0900

    stash folder state

---
+++ frontend/src/hooks/useFolderState.tsx
@@ -0,0 +1,21 @@
+import { useSelector } from "react-redux";
+import { RootReducerType } from "../store/store";
+import { UserStateType } from "../store/User/userReducer";
+
+const initialState: UserStateType = {
+  loggedUser: {
+    id: 0,
+    email: "",
+    username: "",
+    profileImage: "",
+    folders: [],
+  },
+};
+
+export const useFolderState = () => {
+  const folders = useSelector(
+    (state: RootReducerType) =>
+      state.user.loggedUser || initialState.loggedUser
+  );
+  return folders;
+};
