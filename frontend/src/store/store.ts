bc35c549a5d8261f3a3b0e0f38295d206aed832e
Jeong-Woo-Kim <53823950+kjw7953@users.noreply.github.com>
2021-11-10 22:14:02 +0900

    Hotfix/non persistent redux state #65 (#66)
    * change browser storage from session to local
    
    * [feat] add redux-persist and redux-logger
    
    * add @types/redux-persist
    
    * Apply persist-redux

--- frontend/src/store/store.ts
+++ frontend/src/store/store.ts
@@ -3,22 +3,36 @@ import {
   combineReducers,
   createStore,
   compose,
-  applyMiddleware
+  applyMiddleware,
 } from "redux";
 import thunk from "redux-thunk";
+// import logger from 'redux-logger';
+import { persistReducer } from 'redux-persist';
+import storage from 'redux-persist/lib/storage';
 
 import userReducer from './User/userReducer';
 import postReducer from './Post/postReducer';
+
+const persistConfig = {
+  key: 'root',
+  storage,
+  whitelist: ['user']
+}
 
 const rootReducer: Redux.Reducer = combineReducers({
   user: userReducer,
   post: postReducer,
 });
 
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 const store: Redux.Store = createStore(
-  rootReducer,
+  persistedReducer,
   {},
-  compose(applyMiddleware(thunk))
+  compose(applyMiddleware(
+    thunk,
+    // logger
+  ))
 );
 
 export type RootReducerType = ReturnType<typeof rootReducer>;
