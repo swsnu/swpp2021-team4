3212aefd94b088e3fd59b66a869b5fc03c7fac41
mina <dbmina@naver.com>
2021-11-21 14:31:18 +0900

    add search logo

--- frontend/src/containers/Search.tsx
+++ frontend/src/containers/Search.tsx
@@ -1,7 +1,11 @@
 import React from "react";
-
+import search from '../static/search.svg';
 function Search() {
-  return <div></div>;
+  return <div className="search">
+    <img src={search} />
+    <input type="text" placeholder="키워드를 입력해주세요"/>
+    <button>Search</button>
+  </div>;
 }
 
 export default Search;
