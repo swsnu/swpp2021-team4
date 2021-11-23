ae5b807d4150fd5fd927e87f998392e9ea88f89c
kjw7953 <uis7953@naver.com>
2021-11-23 21:48:44 +0900

    Implement path between places and calculate moving time

--- frontend/src/utils/returnPathTime.ts
+++ frontend/src/utils/returnPathTime.ts
@@ -1,14 +1,27 @@
 import axios from "axios";
 
-export default (origin: string, destination: string, type: 'car'|'pub'|'vic'|'wal') => {
-  axios.get(`https://apis-navi.kakaomobility.com/v1/directions?origin=${origin}&destination=${destination}`, {
+export default async (origin: string, destination: string, type: 'car'|'pub'|'vic'|'wal') => {
+  console.log('api called...');
+  let distance: number = 0; // unit: meter
+  let meterPerHour: number =
+    type === 'car' ? 80000 :
+    type === 'pub' ? 40000 :
+    type === 'vic' ? 20000 :
+    type === 'wal' ? 4000 :
+    0;
+  let result: string = '';
+
+  return axios.get(`/v1/directions?origin=${origin}&destination=${destination}`, {
     headers: {
       Authorization: `KakaoAK ${process.env.REACT_APP_KAKAO_NAVI_REST_API_KEY || 'SET_ENV_KEY'}`
     }
-  })
-  .then(res => console.log(res))
-  .catch(err => console.log(err));
-
-
-  return type;
+  }).then(res => {
+    distance = res.data.routes[0]?.summary?.distance;
+    if (distance/meterPerHour >= 1) {
+      const hour = Math.floor(distance/meterPerHour);
+      result += `${hour}시간 `;
+      distance %= hour*meterPerHour;
+    }
+    return result += `${Math.floor(distance/(meterPerHour/60))}분`
+  }).catch(() => '');
 };
