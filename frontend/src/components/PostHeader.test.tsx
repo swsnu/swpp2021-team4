e61c501582edc8ebbcdfe6e7f940cc397c1bb167
young924 <ahenaka9@gmail.com>
2021-11-30 11:59:52 +0900

    Fix code smells and duplicated lines

--- frontend/src/components/PostHeader.test.tsx
+++ frontend/src/components/PostHeader.test.tsx
@@ -29,44 +29,25 @@ describe('<PostHeader />', () => {
     it('should render without errors when not logged in', () => {
         post.comment_counts = 1;
         post.comments = null;
-        const component = shallow(<PostHeader
-            loggedUserId={0}
-            post={post}
-            isPostDetail={false}
-            onClickPostLikeButton={() => { }} />);
+        const component = shallow(<PostHeader loggedUserId={0} post={post} isPostDetail={false} onClickPostLikeButton={jest.fn()} />);
         const wrapper = component.find('.post-header');
         expect(wrapper.length).toBe(1);
     });
 
     it('should render without errors when logged in', () => {
-        let component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickAddPostCartButton={() => { }}
-            onClickPostLikeButton={() => { }} />);
+        let component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickAddPostCartButton={jest.fn()} onClickPostLikeButton={jest.fn()} />);
         let wrapper = component.find('.unliked');
         expect(wrapper.length).toBe(1);
 
         post.liked = true;
 
-        component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickAddPostCartButton={() => { }}
-            onClickPostLikeButton={() => { }} />);
+        component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickAddPostCartButton={jest.fn()} onClickPostLikeButton={jest.fn()} />);
         wrapper = component.find('.liked');
         expect(wrapper.length).toBe(1);
 
         post.is_shared = false;
 
-        component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickAddPostCartButton={() => { }}
-            onClickPostLikeButton={() => { }} />);
+        component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickAddPostCartButton={jest.fn()} onClickPostLikeButton={jest.fn()} />);
         wrapper = component.find('.post-share-button');
         expect(wrapper.length).toBe(1);
     });
@@ -75,44 +56,28 @@ describe('<PostHeader />', () => {
         post.season = 'spr';
         post.theme = 'friends'
 
-        let component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickPostLikeButton={() => { }} />);
+        let component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickPostLikeButton={jest.fn()} />);
         let wrapper = component.find('.season');
         expect(wrapper.text()).toEqual('Spring');
 
         post.season = 'sum';
         post.theme = 'family';
 
-        component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickPostLikeButton={() => { }} />);
+        component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickPostLikeButton={jest.fn()} />);
         wrapper = component.find('.season');
         expect(wrapper.text()).toEqual('Summer');
 
         post.season = 'aut';
         post.theme = 'lover';
 
-        component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickPostLikeButton={() => { }} />);
+        component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickPostLikeButton={jest.fn()} />);
         wrapper = component.find('.season');
         expect(wrapper.text()).toEqual('Autumn');
 
         post.season = 'win';
         post.theme = 'alone';
 
-        component = shallow(<PostHeader
-            loggedUserId={1}
-            post={post}
-            isPostDetail={true}
-            onClickPostLikeButton={() => { }} />);
+        component = shallow(<PostHeader loggedUserId={1} post={post} isPostDetail={true} onClickPostLikeButton={jest.fn()} />);
         wrapper = component.find('.season');
         expect(wrapper.text()).toEqual('Winter');
     });
