cda8f5a6cafc10bf49f200fab12251003b8db24c
mina <dbmina@naver.com>
2021-12-07 18:05:35 +0900

    add Footer & serviceInfo logo

--- frontend/src/components/Footer.tsx
+++ frontend/src/components/Footer.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 
 function Footer() {
-  return <div className="Footer"></div>;
+  return (
+    <div className="Footer">
+      <div className="logo">@Tripick</div>
+      <div className="footer-explanation">Pick your Trip</div>
+    </div>
+  );
 }
 
 export default Footer;
--- frontend/src/containers/ServiceInfo.tsx
+++ frontend/src/containers/ServiceInfo.tsx
@@ -1,11 +1,20 @@
 import React from "react";
-
+import { NavLink } from "react-router-dom";
+import logo from "../static/nav_logo.svg";
 import "../styles/components/ServiceInfo.css";
 
 function ServiceInfo() {
   return (
     <div className="serviceInfo">
-      <div className="service-header">Service Information</div>
+      <div className="explanation">
+        <div className="logo">
+          <NavLink to="/main/">
+            <img className="logo" src={logo} />
+          </NavLink>
+        </div>
+        <div className="service-header">Pick your Trip!</div>
+      </div>
+      <div className="service-content"></div>
     </div>
   );
 }
