b5f4a6decb6d8f358d23a709815554933b72f148
mina <dbmina@naver.com>
2021-12-07 16:36:23 +0900

    add ServiceInfo.tsx & ServiceInfo.css

---
+++ frontend/src/containers/ServiceInfo.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+
+import "../styles/components/ServiceInfo.css";
+
+function ServiceInfo() {
+  return (
+    <div className="serviceInfo">
+      <div className="service-header">Service Information</div>
+    </div>
+  );
+}
+
+export default ServiceInfo;
