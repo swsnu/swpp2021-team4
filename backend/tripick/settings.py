fcc353a0f90508f170e66a1ada28074113ab86c9
young924 <ahenaka9@gmail.com>
2021-12-08 16:58:18 +0900

    allowed host

--- backend/tripick/settings.py
+++ backend/tripick/settings.py
@@ -26,7 +26,7 @@ SECRET_KEY = 'django-insecure-melkc2s@uay!owd#hn=ooi=7d+&kesht!6*q1eha$o3kyd@f-8
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = [ 'ec2-3-36-99-173.ap-northeast-2.compute.amazonaws.com' ]
+ALLOWED_HOSTS = [ 'ec2-3-38-94-224.ap-northeast-2.compute.amazonaws.com' ]
 
 
 # Application definition
