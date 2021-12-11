9edb47cb084d4f80de0c5688ed52b4dc2a871f41
young924 <ahenaka9@gmail.com>
2021-12-11 20:21:21 +0900

    Set proxy

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -1,10 +1,31 @@
 const { createProxyMiddleware } = require('http-proxy-middleware');
 
-module.exports = function(app) {
+module.exports = function (app) {
   app.use(
     '/v1',
     createProxyMiddleware({
       target: 'https://apis-navi.kakaomobility.com',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/user',
+    createProxyMiddleware({
+      target: 'http://ec2-3-38-182-212.ap-northeast-2.compute.amazonaws.com:8000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/place',
+    createProxyMiddleware({
+      target: 'http://ec2-3-38-182-212.ap-northeast-2.compute.amazonaws.com:8000',
+      changeOrigin: true,
+    })
+  );
+  app.use(
+    '/post',
+    createProxyMiddleware({
+      target: 'http://ec2-3-38-182-212.ap-northeast-2.compute.amazonaws.com:8000',
       changeOrigin: true,
     })
   );
