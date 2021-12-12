b98eed86239c53d79100c167a44c61e67200b6d4
young924 <ahenaka9@gmail.com>
2021-12-12 15:46:12 +0900

    Remove backend on setupProxy and modify nginx.config

--- frontend/src/setupProxy.js
+++ frontend/src/setupProxy.js
@@ -8,25 +8,25 @@ module.exports = function (app) {
       changeOrigin: true,
     })
   );
-  app.use(
-    '/user',
-    createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/place',
-    createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
-      changeOrigin: true,
-    })
-  );
-  app.use(
-    '/post',
-    createProxyMiddleware({
-      target: 'http://127.0.0.1:8000',
-      changeOrigin: true,
-    })
-  );
+  // app.use(
+  //   '/user',
+  //   createProxyMiddleware({
+  //     target: 'http://127.0.0.1:8000',
+  //     changeOrigin: true,
+  //   })
+  // );
+  // app.use(
+  //   '/place',
+  //   createProxyMiddleware({
+  //     target: 'http://127.0.0.1:8000',
+  //     changeOrigin: true,
+  //   })
+  // );
+  // app.use(
+  //   '/post',
+  //   createProxyMiddleware({
+  //     target: 'http://127.0.0.1:8000',
+  //     changeOrigin: true,
+  //   })
+  // );
 };
